a89f5746b919f297b2aa773d06f965b309f3ad8b
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-09-27 16:59:57 +0300

    4-darss

---
+++ index.js
@@ -0,0 +1,170 @@
+// 1-misol
+// let res = prompt("Sonni kiriting...");
+
+// let sum = '';
+
+// if (res > 0) {
+// sum = 'Musbat'
+// } else {
+//     sum = 'Manfiy'
+// }
+
+// console.log(sum);
+
+// 2-misol
+
+// let res = prompt("Sonni kiriting...");
+
+// let sum = '';
+
+// if (res % 2 == 0) {
+// sum = 'Juft son'
+// } else {
+//     sum = 'Toq son'
+// }
+
+// console.log(sum);
+
+// 3-misol
+
+// let res = prompt("Sonni kiriting...");
+// let add = prompt("Sonni kiriting...");
+
+// let sum = '';
+
+// if (res > add) {
+// sum = 'Katta'
+// } else {
+//     sum = 'Kichik'
+// }
+
+// console.log(sum);
+
+// 4-misol
+
+// let res = prompt("Sonni kiriting...");
+// let add = prompt("Sonni kiriting...");
+
+// let sum = '';
+
+// if (res == add) {
+// sum = 'Kvadrat'
+// } else {
+//     sum = 'Kvadrat emas'
+// }
+
+// console.log(sum);
+
+// 5-misol
+
+//  let son = prompt("haroratni kiriting...")
+
+//     if (son > 0) {
+//         console.log("Yozgi");
+//     } if (son < 0) {
+//         console.log("Qishki");
+//     }
+
+
+// 6-misol
+
+// let res = prompt("Sonni kiriting...");
+// sum = '';
+
+// if (res == 5) {
+//     sum = 'Alo'
+// } if (res == 4) {
+//     sum = 'yaxshi'
+// } if (res == 3) {
+//     sum = 'Qoniqarli'
+// } if (res == 2) {
+//     sum = 'Yomon'
+// }
+
+// console.log(sum);
+
+// 7-misol
+// let res = prompt("Sonni kiriting...");
+// let res2 = prompt("Sonni kiriting...");
+// let res3 = prompt("Sonni kiriting...");
+ 
+// let max = 0;
+// if (res > res2 ) {
+//     max = res
+// } if (res3 > res) {
+//     max = res3
+// }
+
+// console.log(max);
+
+// 8-misol
+// let son = prompt("Sonni kiriting...")
+
+//     if (son >= 10000 && son <= 99999) {
+//         console.log("Besh xonali");
+//     } else {
+//         console.log("Besh xonali emas");
+//     }
+
+
+// 9-misol
+// let oy = prompt("Sonni kiriting")
+
+//     if (oy >= 3 && oy <= 5) {
+//         console.log("Bahor");
+//     } else if (oy >= 6 && oy <= 8) {
+//         console.log("Yoz");
+//     } else if (oy >= 9 && oy <= 11) {
+//         console.log("Kuz");
+//     } else if (oy === 12 || (oy >= 1 && oy <= 2)) {
+//         console.log("Qish");
+//     } else {
+//         console.log("Noto'g'ri oy raqami");
+//     }
+
+
+// 10-misol
+//  let son = prompt("Sonni kiriting...")
+
+//     if (son > 1, son < 5) {
+//         console.log("Ish kuni");
+//     } if (son == 6, son == 7) {
+//         console.log("Ish kuni emas");
+//     }
+
+// 11-misol
+
+//  let son = prompt("Yoshingizni kiriting...")
+
+//     if (son > 2005) {
+//         console.log("Kichik odam");
+//     } if (son < 2005) {
+//         console.log("Katta odam");
+//     }
+
+// 12-misol
+
+//  let son = prompt("Ish vaqtini kiriting...")
+
+//     if (son > 8, son < 18) {
+//         console.log("Ish vaqti");
+//     } else {
+//         console.log("Tanafus");
+//     }
+
+// 13-misol
+
+//  let son = prompt("sonni kiriting...")
+
+    // if (son > 8, son < 18) {
+    //     console.log("Ish vaqti");
+    // } else {
+    //     console.log("Tanafus");
+    // }
+// 15-misol
+//  let son = prompt("sonni kiriting...")
+//     if (son < 18) {
+//         console.log("Tekin kirish");
+//     } else {
+//         console.log("Pullik kirish");
+//     }
